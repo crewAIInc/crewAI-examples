46c0f6e15170118767941e31e3e47c22ecc6f9e1
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2024-07-04 16:02:02 -0300

    docs: Add code intepreter tool example

---
+++ code_generation_crew/main.py
@@ -0,0 +1,31 @@
+from agents import Agents
+from tasks import Tasks
+
+from crewai import Crew
+
+tasks = Tasks()
+agents = Agents()
+
+# Create Agents
+researcher_agent = agents.quant_financial_agent()
+reporting_analyst_agent = agents.reporting_analyst()
+
+# Define Tasks for each agent
+research_company_culture_task = tasks.research_company_stocks(researcher_agent)
+summarize_stock_task = tasks.summarize_stock_information(researcher_agent)
+
+# Instantiate the crew with a sequential process
+crew = Crew(
+    agents=[researcher_agent, reporting_analyst_agent],
+    tasks=[
+        research_company_culture_task,
+        summarize_stock_task,
+    ],
+)
+
+# Kick off the process
+result = crew.kickoff()
+
+print("Financial Analysis Process Completed.")
+print("Final Job Posting:")
+print(result)
