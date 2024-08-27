e0dcb468303168174abf28a3211cd38010339d43
Bisan Suleiman <bisansuleiman@berkeley.edu>
2024-08-27 11:12:13 -0700

    added main.py

---
+++ job-posting/src/job_posting/main.py
@@ -0,0 +1,37 @@
+import sys
+from job_posting.crew import JobPostingCrew
+
+def run():
+    # Replace with your inputs, it will automatically interpolate any tasks and agents information
+    # inputs = {
+    #     'company_domain':'baskinrobins.com',
+    #     'company_description': 'Baskin-Robbins is an American multinational chain of ice cream and cake specialty shops owned by Inspire Brands',
+    #     'hiring_needs': 'Cake decorator,  at least 1 - 2 years cake decorating experience, stand for long periods of time, cake decorator needs',
+    #     'specific_benefits':'Weekly Pay, Employee Meals',
+    # }
+    # JobPostingCrew().crew().kickoff(inputs=inputs)
+    inputs = {
+        'company_domain':'careers.wbd.com',
+        'company_description': "Warner Bros. Discovery is a premier global media and entertainment company, offering audiences the world’s most differentiated and complete portfolio of content, brands and franchises across television, film, sports, news, streaming and gaming. We're home to the world’s best storytellers, creating world-class products for consumers",
+        'hiring_needs': 'Production Assistant, for a TV production set in Los Angeles in June 2025',
+        'specific_benefits':'Weekly Pay, Employee Meals, healthcare',
+    }
+    JobPostingCrew().crew().kickoff(inputs=inputs)
+
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        'company_domain':'baskinrobins.com',
+        'company_description': 'Baskin-Robbins is an American multinational chain of ice cream and cake specialty shops owned by Inspire Brands',
+        'hiring_needs': 'Cake decorator,  at least 1 - 2 years cake decorating experience, stand for long periods of time, cake decorator needs',
+        'specific_benefits':'Weekly Pay, Employee Meals',
+    }
+    try:
+        JobPostingCrew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
